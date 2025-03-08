ce20d6f83f198ce910a91ff18a66bf21d3c7130e
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-08 12:29:43 +0100

    Update comment.js

--- comment/comment.js
+++ comment/comment.js
@@ -415,25 +415,4 @@ function renderReplies(replies, commentId) {
 }
   
       window.onload = fetchComments;
-  function formatTimeAgo(date) {
-    const seconds = Math.floor((new Date() - date) / 1000);
-
-    let interval = Math.floor(seconds / 31536000);
-    if (interval >= 1) return interval === 1 ? "1 year ago" : `${interval} years ago`;
-
-    interval = Math.floor(seconds / 2592000);
-    if (interval >= 1) return interval === 1 ? "1 month ago" : `${interval} months ago`;
-
-    interval = Math.floor(seconds / 604800);
-    if (interval >= 1) return interval === 1 ? "1 week ago" : `${interval} weeks ago`;
-
-    interval = Math.floor(seconds / 86400);
-    if (interval >= 1) return interval === 1 ? "1 day ago" : `${interval} days ago`;
-
-    interval = Math.floor(seconds / 3600);
-    if (interval >= 1) return interval === 1 ? "1 hour ago" : `${interval} hours ago`;
-
-    interval = Math.floor(seconds / 60);
-    if (interval >= 1) return interval === 1 ? "1 minute ago" : `${interval} minutes ago`;
-
-  }
+  
