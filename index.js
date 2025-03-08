6fe443e988bcea15631cb70c6a0597db9f6ab3fd
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-08 11:36:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'comment', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${port}`);
 });
