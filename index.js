77ea854b968f7bc5a74629102a534c954aeb2eed
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-09 19:01:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
 const formidable = require('formidable');
 const fs = require('fs');
 const { put } = require('@vercel/blob'); // Assuming you're using Vercel Blob's client
+// Serve all static files from the 'comment' folder
+app.use(express.static(path.join(__dirname, 'comment')));
 
+// Route to handle homepage or index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'comment', 'index.html'));
+});
 module.exports.config = {
   api: {
     bodyParser: false, // Disable bodyParser to handle files manually
@@ -35,4 +44,9 @@ module.exports = async function handler(req, res) {
       return res.status(500).json({ error: 'Upload failed', details: error.message });
     }
   });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${port}`);
+});
+
 };
