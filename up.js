8c5138922925d11b60cadb74c871caf18e5f81a8
Netdot12 <netdot1234@gmail.com>
2025-03-09 10:11:16 +0100

    Initial commit

--- up.js
+++ up.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 
 // Instantiate Octokit with authentication token
 const octokit = new Octokit({
-  auth: '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',
+  auth: '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', // your GitHub token
 });
 
 // Path to your file
-const filePath = './package.json';
+const filePath = './okay.js';
 const content = fs.readFileSync(filePath, 'utf8');
 
 // Upload the file to GitHub repository
@@ -16,8 +16,8 @@ async function uploadFile() {
     const response = await octokit.repos.createOrUpdateFileContents({
       owner: 'Netdot12',
       repo: 'netdot12.github.io',
-      path: 'uploads/p.txt', // path in the repo
-      message: 'Upload file message', // commit message
+      path: 'uploads/okay.js', // path in the repo for the new file
+      message: 'Upload okay.js file', // commit message
       content: Buffer.from(content).toString('base64'), // base64 encoded content
       committer: {
         name: 'Netdot12',
@@ -26,7 +26,8 @@ async function uploadFile() {
       author: {
         name: 'Netdot12',
         email: 'netdot1234@gmail.com',
-      }
+      },
+      mode: '100644', // setting file permissions
     });
     console.log('File uploaded successfully:', response.data);
   } catch (error) {
